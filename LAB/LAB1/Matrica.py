23eadd748c82d8795245edc9109d9d6641a2943c
Tin Jukić <Tin.Jukic@fer.hr>
2023-10-16 15:08:35 +0200

    LAB1 methods override

--- LAB/LAB1/Matrica.py
+++ LAB/LAB1/Matrica.py
@@ -49,7 +49,7 @@ class Matrica:
             sys.stderr.write(f"Position out of range\n{error}\n")
             return None
 
-    def set_element_at(self, position: tuple[int, int],  element: float) -> None:
+    def set_element_at(self, position: tuple[int, int], element: float) -> None:
         """
         Sets element at desired position.
         :param position: at which to put the element
@@ -84,3 +84,132 @@ class Matrica:
         except FileNotFoundError:
             sys.stderr.write(f"Provided file does not exist.")
             return None
+
+    def save_matrix_to_file(self, file: str) -> None:
+        """
+        Saves matrix into desired file.
+        :param file: in which to save the matrix
+        :return: None
+        """
+        last_element_index: int = len(self.elements[0]) - 1
+        try:
+            with open(file, 'w', encoding='utf-8') as file_matrix:
+                for row in self.elements:
+                    for j, element in enumerate(row):
+                        if j < last_element_index:
+                            file_matrix.write(f"{str(element)} ")
+                        else:
+                            file_matrix.write(f"{str(element)}\n")
+        except ValueError:
+            sys.stderr.write(f"Value cannot be converted to string.\n")
+
+    def print_matrix(self) -> None:
+        """
+        Prints matrix on screen.
+        :return: None
+        """
+        last_element_index: int = len(self.elements[0]) - 1
+        for row in self.elements:
+            for j, element in enumerate(row):
+                if j < last_element_index:
+                    print(f"{element} ")
+                else:
+                    print(f"{element}\n")
+
+    def __add__(self, other) -> Matrica:
+        """
+        Adds all elements from one matrix to another.
+        :param other: matrix to be added
+        :return: new *Matrica* object
+        """
+        elements: list[list[float]] = []
+        if isinstance(other, float | int):
+            for row in self.elements:
+                col: list[float] = []
+                for element in row:
+                    col.append(element + other)
+                elements.append(col)
+        else:
+            for row1, row2 in zip(self.elements, other.elements):
+                col: list[float] = []
+                for element1, element2 in zip(row1, row2):
+                    col.append(element1 + element2)
+                elements.append(col)
+        return Matrica(elements=elements)
+
+    def __sub__(self, other) -> Matrica:
+        """
+        Subtracts all elements from one matrix to another.
+        :param other: matrix to be subtracted
+        :return: new *Matrix* object
+        """
+        elements: list[list[float]] = []
+        if isinstance(other, float | int):
+            for row in self.elements:
+                col: list[float] = []
+                for element in row:
+                    col.append(element - other)
+                elements.append(col)
+        else:
+            for row1, row2 in zip(self.elements, other.elements):
+                col: list[float] = []
+                for element1, element2 in zip(row1, row2):
+                    col.append(element1 - element2)
+                elements.append(col)
+        return Matrica(elements=elements)
+
+    def __mul__(self, other) -> Matrica:
+        """
+        Multiplies all elements from one matrix to another.
+        :param other: matrix to be multiplied
+        :return: new *Matrica* object
+        """
+        elements: list[list[float]] = []
+        if isinstance(other, float | int):
+            for row in self.elements:
+                col: list[float] = []
+                for element in row:
+                    col.append(element * other)
+                elements.append(col)
+        else:
+            for row1, row2 in zip(self.elements, other.elements):
+                col: list[float] = []
+                for element1, element2 in zip(row1, row2):
+                    col.append(element1 * element2)
+                elements.append(col)
+        return Matrica(elements=elements)
+
+    def __invert__(self) -> Matrica:
+        """
+        Transposes the matrix.
+        :return: new *Matrica* object
+        """
+        elements: list[list[float]] = []
+        width, height = len(self.elements), len((self.elements[0]))
+        for j in range(height):
+            col: list[float] = []
+            for i in range(width):
+                col.append(self.elements[i][j])
+            elements.append(col)
+        return Matrica(elements=elements)
+
+    def __eq__(self, other) -> bool:
+        """
+        Compares two matrices.
+        :param other: matrix to be compared to
+        :return: *True* if matrices are equal, *False* otherwise
+        """
+        if type(self) is not type(other):
+            return False
+
+        width1, height1 = len(self.elements), len(self.elements[0])
+        width2, height2 = len(other.elements), len(other.elements[0])
+        if width1 != width2 or height1 != height2:
+            return False
+
+        for i in range(width1):
+            for j in range(height1):
+                if self.elements[i][j] != other.elements[i][j]:
+                    return False
+
+        return True
